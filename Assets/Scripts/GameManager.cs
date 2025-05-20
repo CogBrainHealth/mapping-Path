6a39a2b21b8413a667934e4e45b49830880fedab
고하영 <124705568+GOHAGONG@users.noreply.github.com>
2025-05-20 22:20:27 +0900

    TMP Emoji Setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -382,17 +382,25 @@ public class GameManager : MonoBehaviour
 
         Scoring();
         Debug.Log(score);
+        GameOverText();
 
+        isGameOver = true; // 마지막 문제에서 시간초과 시 또 호출 되는 것을 방지
+        pathGenerator.gameObject.SetActive(false);
+        touchControl.gameObject.SetActive(false); // 얘네 말고 다른 애들도 비활성화해놓기
+    }
+
+    public void GameOverText()
+    {
         if (score > 25.27)
         {
             messageTitle.text += "20대";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "최고 수준의 기억력!\n" +
-                             dataManager.nickName + "님은 20대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"rocket\">최고 수준의 기억력!\n" +
+                             dataManager.nickName + "님은 20대 수준의 기억력을 가지고\n있어요!\n" +
                              "한 번 본 경로를 거의 완벽하게 기억하고 재현할 수 있는 능력을 가지고 있네요.\n\n" +
-                             "이 수준을 유지하려면?\n" +
-                             "- 경로를 기억할 때 시각적 이미지 (예: 나무, 건물, 표지판 등)를 떠올려 보세요.\n" +
+                             " <sprite name=\"check\">이 수준을 유지하려면?\n" +
+                             "- 경로를 기억할 때 시각적 이미지\n(예: 나무, 건물, 표지판 등)를 떠올려 보세요.\n" +
                              "- 실제 이동한 경로를 종이에 그려보며 복습하는 것도 좋은 방법입니다!";
         }
 
@@ -400,11 +408,11 @@ public class GameManager : MonoBehaviour
         {
             messageTitle.text += "30대";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "우수한 기억력!\n" +
-                             dataManager.nickName + "님은 30대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"gift\">우수한 기억력!\n" +
+                             dataManager.nickName + "님은 30대 수준의 기억력을 가지고\n있어요!\n" +
                              "경로를 빠르게 파악하고 기억하는 능력이 뛰어나며, 실수 없이 재현할 가능성이 높아요.\n\n" +
-                             "이 능력을 더 키우려면?\n" +
+                             " <sprite name=\"check\">이 능력을 더 키우려면?\n" +
                              "- 이동 경로를 머릿속으로 떠올리며 복습하는 습관을 들여보세요.\n" +
                              "- 도착 후 '어떤 길을 어떻게 왔는지' 떠올려보며 점검하는 연습이 효과적입니다.";
         }
@@ -413,11 +421,11 @@ public class GameManager : MonoBehaviour
         {
             messageTitle.text += "40대";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "균형 잡힌 기억력!\n" +
-                             dataManager.nickName + "님은 40대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"good\">균형 잡힌 기억력!\n\n" +
+                             dataManager.nickName + "님은 40대 수준의 기억력을 가지고\n있어요!\n" +
                              "전체 경로를 한 번에 기억하기보다는, 주요 지점(랜드마크 중심)으로 기억하는 경향이 있어요.\n\n" +
-                             "좀 더 향상시키려면?\n" +
+                             " <sprite name=\"check\">좀 더 향상시키려면?\n" +
                              "- 이동하면서 '내가 지난 장소에서 어떤 특징이 있었는지' 떠올리는 습관을 길러보세요.\n" +
                              "- 길을 걸은 후, 일정 시간이 지난 뒤 머릿속으로 경로를 다시 떠올려보는 연습을 추천합니다.";
         }
@@ -426,11 +434,11 @@ public class GameManager : MonoBehaviour
         {
             messageTitle.text += "50대";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "훈련하면 더 좋아질 수 있어요!\n" +
-                             dataManager.nickName + "님은 50대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"running\">훈련하면 더 좋아질 수 있어요!\n" +
+                             dataManager.nickName + "님은 50대 수준의 기억력을 가지고\n있어요!\n" +
                              "길을 찾을 때 여러 번 생각해야 하거나, 중간에 일부 경로를 잊을 가능성이 있어요.\n\n" +
-                             "더 나은 기억력을 원한다면?\n" +
+                             " <sprite name=\"check\">더 나은 기억력을 원한다면?\n" +
                              "- 이동할 때 경로를 하나의 이야기(스토리)로 만들어보세요!\n" +
                              "  예: '큰 나무를 지나면 왼쪽에 있는 빨간 건물을 보고, 그다음 횡단보도를 건넌다.'\n" +
                              "  이처럼 스토리로 연결하면 기억이 더 오래 유지됩니다!";
@@ -441,11 +449,11 @@ public class GameManager : MonoBehaviour
         {
             messageTitle.text += "60대 (8.84점 ~ 13.16점)";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "조금 더 자주 훈련하면 좋아질 수 있어요!\n" +
-                             dataManager.nickName + "님은 60대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"brain\">조금 더 자주 훈련하면 좋아질 수 있어요!\n" +
+                             dataManager.nickName + "님은 60대 수준의 기억력을 가지고\n있어요!\n" +
                              "경로를 기억하는 데 시간이 걸릴 수 있으며, 일부 정보가 빠르게 사라질 수 있어요.\n\n" +
-                             "훈련을 위해?\n" +
+                             " <sprite name=\"check\">훈련을 위해?\n" +
                              "- 작은 구간별로 이동 후, 10초 동안 멈춰서 '내가 방금 어디를 지나왔는지; 떠올려보세요.\n" +
                              "- 종이에 경로를 직접 그려보는 것도 기억력 강화에 효과적입니다!";
 
@@ -455,23 +463,20 @@ public class GameManager : MonoBehaviour
         {
             messageTitle.text += " 70대 (4점 ~ 8.84점)";
 
-            message.text += "길찾기 게임을 통해 기억력을 검사했어요!\n\n" +
-                             "조금 더 연습하면 기억력이 향상될 거예요!\n" +
-                             dataManager.nickName + "님은 70대 수준의 기억력을 가지고 있어요!\n" +
+            message.text += "길찾기 게임을 통해 기억력을\n검사했어요!\n\n" +
+                             " <sprite name=\"weight\">조금 더 연습하면 기억력이 향상될 거예요!\n" +
+                             dataManager.nickName + "님은 70대 수준의 기억력을 가지고\n있어요!\n" +
                              "경로를 기억하는 것이 다소 어려울 수 있지만, 반복 학습을 통해 충분히 좋아질 수 있어요.\n\n" +
-                             "훈련을 위해?\n" +
+                             " <sprite name=\"check\">훈련을 위해?\n" +
                              "- 짧고 간단한 경로부터 시작해 기억하는 연습을 해보세요.\n" +
                              "- 예를 들어, 집에서 편의점까지의 길을 기억한 뒤, 거리를 점점 늘려가는 방식이 효과적입니다!";
         }
 
         score /= 26; // 만점이 26점
-        scoreText.text += (score * 100).ToString("F2");
         scoreGage.fillAmount = score;
-        Debug.Log(score);
-
-        isGameOver = true; // 마지막 문제에서 시간초과 시 또 호출 되는 것을 방지
-        pathGenerator.gameObject.SetActive(false);
-        touchControl.gameObject.SetActive(false); // 얘네 말고 다른 애들도 비활성화해놓기
+        score *= 100;
+        scoreText.text += score.ToString("F2");
+        Debug.Log("점수: " + score);
     }
 
     public void Exit()
